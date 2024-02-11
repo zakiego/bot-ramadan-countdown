b26cda655f7c0c29094f789cf08a155aa9258ef0
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-12 00:22:06 +0800

    feat: add env schema

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,18 @@
 import { TwitterApi } from "twitter-api-v2";
+import { z } from "zod";
+
+const envSchema = z.object({
+	TWITTER_ACCESS_TOKEN: z.string(),
+	TWITTER_ACCESS_SECRET: z.string(),
+	TWITTER_APP_KEY: z.string(),
+	TWITTER_APP_SECRET: z.string(),
+});
+
+const env = envSchema.parse(process.env);
 
 export const client = new TwitterApi({
-	accessToken: process.env.TWITTER_ACCESS_TOKEN,
-	accessSecret: process.env.TWITTER_ACCESS_SECRET,
-	appKey: process.env.TWITTER_APP_KEY,
-	appSecret: process.env.TWITTER_APP_SECRET,
+	accessToken: env.TWITTER_ACCESS_TOKEN,
+	accessSecret: env.TWITTER_ACCESS_SECRET,
+	appKey: env.TWITTER_APP_KEY,
+	appSecret: env.TWITTER_APP_SECRET,
 });
